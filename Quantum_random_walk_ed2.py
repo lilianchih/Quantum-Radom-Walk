5725691cef09cbfb949e01b8962d533516cbaac2
lilianchih <lilianchih@gmail.com>
2016-12-30 01:31:16 +0800

    simulate quantum random walk

---
+++ Quantum_random_walk_ed2.py
@@ -0,0 +1,44 @@
+import numpy as np
+from math import sqrt
+import matplotlib.pyplot as plt
+
+mixing_operator = (1/sqrt(2)) * np.array([[1., 1.], [1., -1.]])
+initial_state = np.array([1/(4-2*sqrt(2)), (-1+sqrt(2))/(4-2*sqrt(2))])
+initial_position = 0
+total_steps = 1000
+psi = [] #spin states tensor-product spacial states
+for i in range(2*total_steps + 1):
+    psi.append(np.array([0., 0.]))
+psi[total_steps] = np.copy(initial_state)
+
+def U(state, step):
+    """spin-selective walking operator
+    :type state: a list of np arrays
+    """
+    t = len(state)
+    for j in range(t/2-step+1, t/2+step):
+        state[j] = np.dot(mixing_operator, state[j])
+    temp = np.copy(state)
+    for j in range(t/2-step+1, t/2+step):
+        state[j-1][1] += temp[j][1]
+        state[j][1]  -= temp[j][1]
+        state[j+1][0] += temp[j][0]
+        state[j][0] -= temp[j][0]
+
+for i in range(total_steps):
+    U(psi, i+1)
+
+x = []
+prob = []
+for i in range(2*total_steps+1):
+    x.append(i - total_steps)
+    prob.append((psi[i][0])**2 + (psi[i][1])**2)
+
+deviation = 0
+for i in range(2*total_steps+1):
+    deviation += x[i]**2 * prob[i]
+deviation = sqrt(deviation)
+print deviation
+
+plt.plot(x, prob)
+plt.show()
---
+++ README.md
@@ -0,0 +1,7 @@
+Simulation of quantum random walk
+
+Try other initial states:
+[1., 0.]
+[1/sqrt(2), -1/sqrt(2)]
+
+From this simulation, we can see that the standard deviation ~ total_steps / 2. This is different from a classical random walk, which has standard deviation ~ sqrt(total_steps).
